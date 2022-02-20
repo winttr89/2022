a13c2645c659049e5955152cf08b7d14598015b1
winttr89 <89454761+winttr89@users.noreply.github.com>
2022-02-20 21:47:46 +0700

    Update startvn.sh

--- startvn.sh
+++ startvn.sh
@@ -3,5 +3,5 @@ sudo apt update
 sudo apt upgrade -y 
 sudo apt-get install -y tmux 
 cd /tmp 
-wget https://raw.githubusercontent.com/vnxxx/vnxxx/main/start.sh
+wget https://raw.githubusercontent.com/winttr89/2022/main/start.sh
 sudo -H -u azureuser bash -c 'tmux new-session -d -s install "bash /tmp/start.sh"'
