6abec6977ddb39a197d475948cbebffd6e136822
winttr89 <89454761+winttr89@users.noreply.github.com>
2022-03-10 02:25:49 +0700

    Delete startvn1.sh

--- startvn1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-sudo apt update 
-sudo apt upgrade -y 
-sudo apt-get install -y tmux 
-cd /tmp 
-wget https://raw.githubusercontent.com/winttr89/2022/main/start1.sh
-sudo -H -u azureuser bash -c 'tmux new-session -d -s install "bash /tmp/start.sh"'
