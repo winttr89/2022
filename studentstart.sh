7154d269b0d46d5a7f601e5d32adde701904d081
winttr89 <89454761+winttr89@users.noreply.github.com>
2022-03-14 05:43:31 +0700

    Add files via upload

---
+++ studentstart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sleep 3m
+x=1
+while [ $x -le 500 ]
+do
+  echo "Start vps lan $x"
+  az vm start --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
+  sleep 1m
+  x=$(( $x + 1 ))
+done
