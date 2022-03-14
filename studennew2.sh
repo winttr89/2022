e2c1aafc4e67c1141867d09a027bab5d324df594
winttr89 <89454761+winttr89@users.noreply.github.com>
2022-03-14 10:08:01 +0700

    Update studennew2.sh

--- studennew2.sh
+++ studennew2.sh
@@ -17,7 +17,7 @@ x=1
 while [ $x -le 500 ]
 do
   echo "Start vps lan $x"
-  az vm start --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
+  az vm start --ids $(az vm list -g Server --query "[].id" -o tsv) --no-wait
   echo "Run script lan $x"
   az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --ids $(az vm list -d --query "[?powerState=='VM running'].id" -o tsv) --settings '{"fileUris": ["https://raw.githubusercontent.com/winttr89/2022/main/student.sh"],"commandToExecute": "./student.sh"}'  --no-wait  
   for vps in switzerlandnorth southeastasia eastus eastus2 westus2 northeurope westeurope australiaeast centralindia uksouth koreacentral francecentral
