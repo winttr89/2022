827617f141f77e08ffd2b723c6924362a1c0d54a
winttr89 <89454761+winttr89@users.noreply.github.com>
2022-03-14 07:27:03 +0700

    Update studentstart.sh

--- studentstart.sh
+++ studentstart.sh
@@ -3,7 +3,7 @@ x=1
 while [ $x -le 500 ]
 do
   echo "Start vps lan $x"
-  az vm start --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
+  az vmss start --ids $(az vmss list --resource-group my2021 --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
   sleep 1m
   x=$(( $x + 1 ))
 done
