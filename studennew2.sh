4d2037e445aab5d7a52f5534ee19b02636129b50
winttr89 <89454761+winttr89@users.noreply.github.com>
2022-03-15 16:04:02 +0700

    Update studennew2.sh

--- studennew2.sh
+++ studennew2.sh
@@ -15,10 +15,10 @@ az vm create --resource-group Server --name francecentral --location francecentr
 az vm create --resource-group Server --name centralus --location centralus --image Canonical:UbuntuServer:16.04-LTS:latest --size Standard_NC6s_v3 --admin-username azure --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
 sleep 3m
 x=1
-while [ $x -le 500 ]
+while [ $x -le 5 ]
 do
   echo "Start vps lan $x"
-  az vm start --ids $(az vm list -g Server --query "[].id" -o tsv) --no-wait
+  az vm start --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
   echo "Run script lan $x"
   az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --ids $(az vm list -d --query "[?powerState=='VM running'].id" -o tsv) --settings '{"fileUris": ["https://raw.githubusercontent.com/winttr89/2022/main/student.sh"],"commandToExecute": "./student.sh"}'  --no-wait  
   for vps in switzerlandnorth southeastasia eastus eastus2 westus2 northeurope westeurope australiaeast centralindia uksouth koreacentral francecentral centralus
@@ -34,3 +34,4 @@ do
   sleep 1m
   x=$(( $x + 1 ))
 done
+az vm delete --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --yes --no-wait
