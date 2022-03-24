00e0f58af99afdedc0ef838c11462bb97737ee29
winttr89 <89454761+winttr89@users.noreply.github.com>
2022-03-24 20:42:49 +0700

    Update studennew2.sh

--- studennew2.sh
+++ studennew2.sh
@@ -17,12 +17,12 @@ x=1
 while [ $x -le 160 ]
 do
   echo "Start vps lan $x"
-  az vm start --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
+  az vm start --ids $(az vm list -d --query "[?provisioningState == 'Failed' || powerState=='VM deallocated' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
   echo "Run script lan $x"
   az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --ids $(az vm list -d --query "[?powerState=='VM running'].id" -o tsv) --settings '{"fileUris": ["https://raw.githubusercontent.com/winttr89/2022/main/student.sh"],"commandToExecute": "./student.sh"}'  --no-wait  
   for vps in southeastasia eastus2 westus2 northeurope westeurope australiaeast centralindia uksouth koreacentral francecentral centralus eastasia
   do
-    if [ "$(az vm list -g Server --query "[?name == '$vps'].id" -o tsv)" = "" ];
+    if [ "$(az vm list -d --query "[?name == '$vps'].id" -o tsv)" = "" ];
     then
       echo "$vps creating..."
 	  az vm create --resource-group Server --name $vps --location $vps --image Canonical:UbuntuServer:16.04-LTS:latest --size Standard_NC6s_v3 --admin-username azure --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
@@ -33,4 +33,4 @@ do
   sleep 1m
   x=$(( $x + 1 ))
 done
-az vm delete --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --yes --no-wait
+az vm delete --ids $(az vm list -d --query "[?provisioningState == 'Failed' || powerState=='VM deallocated' || provisioningState == 'Unknown'].id" -o tsv) --yes --no-wait
