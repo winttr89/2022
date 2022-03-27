246876d9defa100fffc405758e5dd31046e763f8
winttr89 <89454761+winttr89@users.noreply.github.com>
2022-03-28 01:19:04 +0700

    Update studentstart.sh

--- studentstart.sh
+++ studentstart.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 x=1
-while [ $x -le 90 ]
+while [ $x -le 60 ]
 do
   echo "Start vps lan $x"
   az vm start --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
   echo "Run script lan $x"
   az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --ids $(az vm list -d --query "[?powerState=='VM running'].id" -o tsv) --settings '{"fileUris": ["https://raw.githubusercontent.com/winttr89/2022/main/student.sh"],"commandToExecute": "./student.sh"}'  --no-wait  
-  for vps in switzerlandnorth southeastasia eastus eastus2 westus2 northeurope westeurope australiaeast centralindia uksouth koreacentral francecentral centralus
+  for vps in southeastasia eastus2 westus2 westeurope centralus
   do
     if [ "$(az vm list -g Server --query "[?name == '$vps'].id" -o tsv)" = "" ];
     then
@@ -15,7 +16,7 @@ do
       echo "$vps was found."
     fi
   done  
-  sleep 1m
+  sleep 2m
   x=$(( $x + 1 ))
 done
 az vm delete --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --yes --no-wait
