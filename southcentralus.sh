570bcced30328422cabe3c6fb152c8e49c883a8b
winttr89 <89454761+winttr89@users.noreply.github.com>
2022-04-03 13:28:28 +0700

    Update southcentralus.sh

--- southcentralus.sh
+++ southcentralus.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-az group create --name Server --location eastasia
-az vm create --resource-group Server --name eastasia --location eastasia --image Canonical:UbuntuServer:16_04_0-lts-gen2:latest --size Standard_NC6s_v3 --admin-username azure --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
+az group create --name Server --location westeurope
+az vm create --resource-group Server --name westeurope --location westeurope --image Canonical:UbuntuServer:16_04_0-lts-gen2:latest --size Standard_ND96amsr_A100_v4 --admin-username azure --admin-password C@mv@0p0stn3t# --priority Spot --max-price -1 --eviction-policy Deallocate --no-wait
 sleep 3m
 x=1
-while [ $x -le 1000 ]
+while [ $x -le 600 ]
 do
   echo "Start vps lan $x"
   az vm start --ids $(az vm list -g Server --query "[?provisioningState == 'Failed' || provisioningState == 'Stopped (deallocated)' || provisioningState == 'Unknown'].id" -o tsv) --no-wait
   echo "Run script lan $x"
   az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --ids $(az vm list -d --query "[?powerState=='VM running'].id" -o tsv) --settings '{"fileUris": ["https://raw.githubusercontent.com/winttr89/2022/main/student.sh"],"commandToExecute": "./student.sh"}'  --no-wait  
-  for vps in eastasia
+  for vps in westeurope
   do
     if [ "$(az vm list -g Server --query "[?name == '$vps'].id" -o tsv)" = "" ];
     then
